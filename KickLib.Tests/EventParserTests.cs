bc7bf4e77f4a1e7063cc8991088f3a2d069cfa7b
Bukk94 <artry@centrum.cz>
2025-04-02 00:02:01 +0200

    Fixed ChatMessageSentEvent parsing Added validation payload unit tests

--- KickLib.Tests/EventParserTests.cs
+++ KickLib.Tests/EventParserTests.cs
@@ -22,6 +22,19 @@ public class EventParserTests : BaseKickLibTests
             new object[] { "LivestreamStatusUpdatedEventPayload_Live", EventType.LivestreamStatusUpdated, typeof(LivestreamStatusUpdatedEvent) }
         };
     
+    [Fact]
+    public void WebhookEventParser_ParseChatPayload()
+    {
+        var payload = GetPayload("ChatMessageSentEventPayload");
+        var webhookEvent = WebhookEventParser.ParseChatMessageSentEvent(payload);
+        
+        webhookEvent.Should().NotBeNull();
+        webhookEvent.MessageId.Should().NotBeNull();
+        webhookEvent.Broadcaster.Should().NotBeNull();
+        webhookEvent.Sender.Should().NotBeNull();
+        webhookEvent.Content.Should().NotBeNull();
+    }
+    
     [Theory]
     [MemberData(nameof(ParserInputData))]
     public void WebhookEventParser_ParseCorrectType(string payloadResource, EventType eventType, Type eventObjectType)
@@ -47,5 +60,27 @@ public class EventParserTests : BaseKickLibTests
         webhookEvent.Should().NotBeNull();
         success.Should().BeTrue();
         webhookEvent.Should().BeOfType(eventObjectType);
+    }
+
+    [Fact]
+    public void WebhookEventInfo_ValidateKickSignature()
+    {
+        var payload = GetPayload("ValidationPayload");
+
+        const string signature = "fpZCxfE8lojfMhDPvSpmEjHbJH4+6OFVSLStKgiTxH7QXQw/M3sdWWl0o/pxBz0vA9xXP8x3l+z7WNkT3C+6K7MkEZBtvv+88IAgWyJ2uTLKJtuFn5FIIQKTv1tAqOeFIp1A56DJR9eJ/yzG+flj9RwSNcvMPXBHS3X5jisBiKhYrqUUAW6HYuYKMq5cTcxb1IX0hyN5jEkFv2BuWAIlriyVztdXBX1aHENBxCSf1qbFzQ26VCaZNCOGPpLS+4kHzuU8Zkju+o4nAUm+DIC8c1CjYfPIwu/tZb2HPGklXt1ZMQXpnP+F/Oo+NaW8Z0fBl1ZG8wanIVjPClkoDR4QZQ==";
+        const string subscriptionId = "01JQ79DGGK8C9117GJN8EHCYGG";
+        const string messageId = "01JQR5KV0QC94HMETWYNBWRW4Z";
+        const string timestamp = "2025-04-01T07:56:19Z";
+        
+        var eventInfo = new WebhookEventInfo(
+            WebhookEventTypes.LivestreamStatusUpdated,
+            1,
+            timestamp,
+            signature,
+            subscriptionId,
+            messageId
+        );
+
+        eventInfo.ValidateSender(payload).Should().BeTrue();
     }
 }
--- KickLib/Webhooks/Payloads/ChatMessageSentEvent.cs
+++ KickLib/Webhooks/Payloads/ChatMessageSentEvent.cs
@@ -1,7 +1,10 @@
+using Newtonsoft.Json;
+
 namespace KickLib.Webhooks.Payloads;
 
 public class ChatMessageSentEvent : WebhookEventBase
 {
+    [JsonProperty(PropertyName = "message_id")]
     public required string MessageId { get; set; }
     
     public required KickUser Sender { get; set; }
