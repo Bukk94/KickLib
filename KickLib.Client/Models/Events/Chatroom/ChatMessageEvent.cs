eb55ef83d0526ce35ce3031b504414725512d0c4
Yalçın GÖRMEZ <3812908+GormYa@users.noreply.github.com>
2025-07-23 12:27:53 +0300

    Add metadata for replied messages
    Added metadata to retrieve original message content and original sender when messages are replied to

--- KickLib.Client/Models/Events/Chatroom/ChatMessageEvent.cs
+++ KickLib.Client/Models/Events/Chatroom/ChatMessageEvent.cs
@@ -1,3 +1,4 @@
+﻿using KickLib.Client.Models.Events.Chatroom.MessageReply;
 using Newtonsoft.Json;
 
 namespace KickLib.Client.Models.Events.Chatroom;
@@ -12,11 +13,15 @@ public class ChatMessageEvent
     public required string Content { get; set; }
 
     // TODO: Convert to enum when all possible types are known
-    // Known values: message
+    // Known values: message, reply
     public required string Type { get; set; }
 
     [JsonProperty(PropertyName = "created_at")]
     public DateTime CreatedAt { get; set; }
 
     public required MessageSender Sender { get; set; }
+
+    // Optional metadata
+    // Known values: original message data
+    public MessageMetadata? Metadata { get; set; }
 }
---
+++ KickLib.Client/Models/Events/Chatroom/MessageReply/MessageMetadata.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace KickLib.Client.Models.Events.Chatroom.MessageReply;
+
+public class MessageMetadata
+{
+    public required string Id { get; set; }
+
+    [JsonProperty(PropertyName = "original_sender")]
+    public required OriginalMessageSender OriginalSender { get; set; }
+
+    [JsonProperty(PropertyName = "original_message")]
+    public required OriginalMessage OriginalMessage { get; set; }
+
+    [JsonProperty(PropertyName = "message_ref")] 
+    public required string MessageReference { get; set; }
+}
